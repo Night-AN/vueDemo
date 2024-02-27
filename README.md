3759895a05c6fa1e1ff93a0e8f30867d4f0a73c1
Night-AN <2754781659@qq.com>
2024-02-27 11:00:04 +0800

    initialized

---
+++ README.md
@@ -0,0 +1,11 @@
+# VueDemo (Updating)
+
+*Environment*
+
+>ez things(configured by yourself)
+
+*PREVIEW*（TODO）
+
+>pnpm install
+
+>node main.js
--- main.js
+++ main.js
@@ -6,13 +6,13 @@
 const http = require('http');
 const fs = require("fs");
 
-const hostname = '127.0.0.1';
+const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  fs.readFile('./index.html', (err, data) => {
+  fs.readFile('./VueEvent/index.html', (err, data) => {
     if (err) throw err;
     console.log(data.toString);
     res.end(data);
